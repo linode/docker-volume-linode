84cac216f82845fc847c04aed624db316a09b1ac
Ricardo Ramirez <ricardo@rhamerica.com>
2018-07-04 05:37:11 +0000

    using libgolang/config

--- main.go
+++ main.go
@@ -1,46 +1,31 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/docker/go-plugins-helpers/volume"
+	"github.com/libgolang/config"
 	"github.com/libgolang/docker-volume-linode/linode"
 	"github.com/libgolang/log"
 )
 
 var (
-	token            string
-	logLevelParamPtr = param("log.leve", "DEBUG", "Log Level")
-	logTraceParamPtr = flag.Bool("log.trace", false, "Set Tracing to true")
-
-	linodeTokenParamPtr = param("linode.token", "", "Required Personal Access Token generated in Linode Console.")
-	hostParamPtr        = param("host", hostname(), "Sets the cluster region")
-	regionParamPtr      = param("region", "us-west", "Sets the cluster region")
+	token               string
+	logLevelParamPtr    = config.String("log.leve", "INFO", "Log Level. Defaults to WARN")
+	logTraceParamPtr    = config.Bool("log.trace", false, "Set Tracing to true")
+	linodeTokenParamPtr = config.String("linode.token", "", "Required Personal Access Token generated in Linode Console.")
+	hostParamPtr        = config.String("linode.host", hostname(), "Sets the cluster region")
+	regionParamPtr      = config.String("linode.region", "us-west", "Sets the cluster region")
 )
 
 func main() {
-	flag.Parse()
+	config.AppName = "docker-volume-linode"
+	config.Parse()
 
 	//
-	//
-	log.SetTrace(true)
-	w := &log.WriterStdout{}
-	w.SetLevel(log.DEBUG)
-	log.SetWriters([]log.Writer{w})
-
-	//
-	// Get token from `-linode.token` param or from LINODE_TOKEN environment variable
-	//
-	if *linodeTokenParamPtr != "" {
-		token = *linodeTokenParamPtr
-	} else {
-		token = os.Getenv("LINODE_TOKEN")
-	}
-	os.Setenv("LOG_CONFIG", "")
-	log.Debug("Using linode token: %s", token)
+	log.GetDefaultWriter().SetLevel(log.StrToLevel(*logLevelParamPtr))
+	log.SetTrace(*logTraceParamPtr)
 
 	// Linode API instance
 	linodeAPI := linode.NewAPI(token, *regionParamPtr, *hostParamPtr)
@@ -51,18 +36,6 @@ func main() {
 	// Attach Driver to docker
 	handler := volume.NewHandler(driver)
 	fmt.Println(handler.ServeUnix("linode-driver", 0))
-}
-
-func param(key, def, help string) *string {
-	envKey := strings.Replace(strings.ToUpper(key), ".", "_", -1)
-	return flag.String(key, getEnv(envKey, def), help)
-}
-
-func getEnv(key, def string) string {
-	if val, ok := os.LookupEnv(key); ok {
-		return val
-	}
-	return def
 }
 
 func hostname() string {
