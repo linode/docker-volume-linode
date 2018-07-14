8e73a899422d96e009a93c6cc4727ec75e3986c0
Ricardo Ramirez <ricardo@rhamerica.com>
2018-07-14 20:40:55 +0000

    Added deploy-latest step

--- scripts/common.sh
+++ scripts/common.sh
@@ -6,30 +6,57 @@ export PLUGIN_NAME_ROOTFS=docker-volume-linode:rootfs.${TRAVIS_BUILD_NUMBER}
 # e.g: docker-volume-linode:master.30
 # e.g: docker-volume-linode:v1.1.30
 export PLUGIN_NAME=libgolang/docker-volume-linode:${TRAVIS_BRANCH}.${TRAVIS_BUILD_NUMBER}
+export PLUGIN_NAME_LATEST=libgolang/docker-volume-linode:latest
+
 
 # Build Step
 build () {
-	# compile
+	compile
+	assemble-plugin-dir
+	create-plugin-from-dir ${PLUGIN_NAME}
+}
+
+# Build Latest Step
+build-latest () {
+	compile
+	assemble-plugin-dir
+	create-plugin-from-dir ${PLUGIN_NAME_LATEST}
+}
+
+# Deploy Step
+deploy () {
+	# Login to docker
+	echo "$DOCKER_PASSWORD" | docker login -u libgolang --password-stdin
+
+	# Push image
+	docker plugin push ${PLUGIN_NAME}
+}
+
+# Deploy Latest Tag Step
+deploy-latest () {
+	# Login to docker
+	echo "$DOCKER_PASSWORD" | docker login -u libgolang --password-stdin
+
+	# Push image
+	docker plugin push ${PLUGIN_NAME_LATEST}
+}
+
+compile () {
 	go get -u github.com/golang/dep/cmd/dep
 	dep ensure
 	docker build --no-cache -q -t ${PLUGIN_NAME_ROOTFS} .
+}
 
+asseble-plugin-dir () {
 	# create plugin
 	mkdir -p ./plugin/rootfs
 	docker create --name tmp  ${PLUGIN_NAME_ROOTFS}
 	docker export tmp | tar -x -C ./plugin/rootfs
 	cp config.json ./plugin/
 	docker rm -vf tmp
-	docker plugin rm -f ${PLUGIN_NAME} || true
-	docker plugin create ${PLUGIN_NAME} ./plugin
 }
 
-
-# Deploy Step
-deploy() {
-	# Login to docker
-	echo "$DOCKER_PASSWORD" | docker login -u libgolang --password-stdin
-
-	# Push image
-	docker plugin push ${PLUGIN_NAME}
+create-plugin-from-dir () {
+	docker plugin rm -f $1 || true
+	docker plugin create $1 ./plugin
 }
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+# set:
+#   -e: Exit on error.
+#   -x: Display commands.
+set -ex
+
+# Tools
+go get -u github.com/tsenart/deadcode
+go get -u github.com/kisielk/errcheck
+go get -u golang.org/x/lint/golint
+
 # Run Code Tests
+go vet
+errcheck
+golint
+deadcode
 go test
 
 # Test Plugin Functionality
